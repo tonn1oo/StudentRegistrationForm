09cef902187db4f42153b28730e2b5ca8ea78903
tonn1oo <ant.vlasov.87@gmail.com>
2022-04-05 20:58:15 +0400

    add fillFormTest

--- src/test/java/demo/qa/StRegFormTests.java
+++ src/test/java/demo/qa/StRegFormTests.java
@@ -1,13 +1,33 @@
 package demo.qa;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class StRegFormTests {
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.holdBrowserOpen = true;
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+    }
+
     @Test
     void fillFormTest(){
-        open("https://demoqa.com/automation-practice-form");
+        open("/automation-practice-form");
+
+        $("[id=firstName]").setValue("Anton");
+        $("[id=lastName]").setValue("Vlasov");
+        $("[id=userEmail]").setValue("qwerty@gmail.com");
+        $("[id=userNumber]").setValue("+79123456");
+        $("[class=custom-control-label]").click();
+
+
+
+
 
     }
 }
