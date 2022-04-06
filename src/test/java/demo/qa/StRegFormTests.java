0ab69c536250214b0e1b51b410fa944a846a70e0
tonn1oo <ant.vlasov.87@gmail.com>
2022-04-07 03:09:49 +0400

    added executeJavaScript,Selenide.zoom

--- src/test/java/demo/qa/StRegFormTests.java
+++ src/test/java/demo/qa/StRegFormTests.java
@@ -1,6 +1,7 @@
 package demo.qa;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -13,7 +14,7 @@ public class StRegFormTests {
     static void setUp() {
         Configuration.holdBrowserOpen = true;
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
+        Configuration.browserSize = "1980x1080";
 
 
     }
@@ -21,11 +22,15 @@ public class StRegFormTests {
     @Test
     void fillFormTest(){
         open("/automation-practice-form");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+        Selenide.zoom(0.85);
+
 
         $("#firstName").setValue("Anton");
         $("#lastName").setValue("Vlasov");
         $("#userEmail").setValue("qwerty@wsdx.ru");
-        $("#userNumber").setValue("+7912345623");
+        $("#userNumber").setValue("7912345623");
         $("#genterWrapper").$(byText("Male")).click();
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOptionByValue("5");
@@ -35,6 +40,14 @@ public class StRegFormTests {
         $("#hobbiesWrapper").$(byText("Sports")).click();
         $("#uploadPicture").uploadFromClasspath("1111.png");
         $("#currentAddress").setValue("Georgia");
+        $("#state").click();
+        $("#stateCity-wrapper").find(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").find(byText("Delhi")).click();
+        $("#submit").click();
+
+
+
 
 
 
