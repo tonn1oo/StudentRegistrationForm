722a68a214b700f25bd2a77245cf859ef206cca9
tonn1oo <ant.vlasov.87@gmail.com>
2022-04-07 09:31:04 +0400

    tests passed

--- src/test/java/demo/qa/StRegFormTests.java
+++ src/test/java/demo/qa/StRegFormTests.java
@@ -5,6 +5,7 @@ import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -15,7 +16,6 @@ public class StRegFormTests {
         Configuration.holdBrowserOpen = true;
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1980x1080";
-
 
     }
 
@@ -33,9 +33,9 @@ public class StRegFormTests {
         $("#userNumber").setValue("7912345623");
         $("#genterWrapper").$(byText("Male")).click();
         $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").selectOptionByValue("5");
-        $(".react-datepicker__year-select").selectOptionByValue("1912");
-        $(".react-datepicker__day--012").click();
+        $(".react-datepicker__month-select").selectOptionByValue("4");
+        $(".react-datepicker__year-select").selectOptionByValue("1987");
+        $(".react-datepicker__month").$(byText("29")).click();
         $("#subjectsInput").setValue("Computer Science").pressEnter();
         $("#hobbiesWrapper").$(byText("Sports")).click();
         $("#uploadPicture").uploadFromClasspath("1111.png");
@@ -45,6 +45,14 @@ public class StRegFormTests {
         $("#city").click();
         $("#stateCity-wrapper").find(byText("Delhi")).click();
         $("#submit").click();
+        ////
+        $(".table-responsive").shouldHave(text("Student Name Anton Vlasov"),
+                text("Student Email qwerty@wsdx.ru"), text("Gender Male"), text("Mobile 7912345623"),
+                text("Date of Birth 29 April,1987"), text("Subjects Computer Science"), text("Hobbies Sports"),
+                text("Picture 1111.png"), text("Address Georgia"), text("State and City NCR Delhi"));
+        /////
+       $("#closeLargeModal").click();
+
 
 
 
