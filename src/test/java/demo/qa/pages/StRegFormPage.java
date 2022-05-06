bea6956f1e54ec5aae14a0b3f24525154efdeb23
tonn1oo <ant.vlasov.87@gmail.com>
2022-05-06 16:32:44 +0300

    added step

--- src/test/java/demo/qa/pages/StRegFormPage.java
+++ src/test/java/demo/qa/pages/StRegFormPage.java
@@ -17,102 +17,107 @@ public class StRegFormPage {
     SelenideElement firstNameInput = $("#firstName");
 
     public StRegFormPage openPage() {
-        open("/automation-practice-form");
-        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
-        executeJavaScript("$('#fixedban').remove()");
-        executeJavaScript("$('footer').remove()");
-        Selenide.zoom(0.85);
-
+        step("Open registration form", () -> {
+            open("/automation-practice-form");
+            $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+            executeJavaScript("$('#fixedban').remove()");
+            executeJavaScript("$('footer').remove()");
+            Selenide.zoom(0.85);
+        });
         return this;
     }
 
-    public StRegFormPage setFirstName(String firstName) {
-        firstNameInput.setValue(firstName);
 
-        return this;
+
+
+        public StRegFormPage setFirstName (String firstName){
+            firstNameInput.setValue(firstName);
+
+            return this;
+        }
+
+        public StRegFormPage setLastName (String lastName){
+            $("#lastName").setValue(lastName);
+
+            return this;
+        }
+
+        public StRegFormPage setEmail (String email){
+            $("#userEmail").setValue(email);
+
+            return this;
+        }
+
+        public StRegFormPage setCurrentAddress (String currentAddress){
+            $("#currentAddress").setValue(currentAddress);
+
+            return this;
+        }
+
+        public StRegFormPage setMobile (String mobile){
+            $("#userNumber").setValue(mobile);
+
+            return this;
+        }
+
+        public StRegFormPage setGender (String value){
+            $("#genterWrapper").$(byText(value)).click();
+
+            return this;
+
+        }
+
+        public StRegFormPage setBirthDate (String day, String month, String year){
+            $("#dateOfBirthInput").click();
+            calendar.setDate(day, month, year);
+
+            return this;
+        }
+
+        public StRegFormPage setSubjects (String value){
+            $("#subjectsInput").setValue(value).pressEnter();
+
+            return this;
+        }
+
+        public StRegFormPage setHobbies (String value){
+            $("#hobbiesWrapper").$(byText(value)).click();
+
+            return this;
+        }
+
+        public StRegFormPage setPicture (String value){
+            $("#uploadPicture").uploadFromClasspath("1111.png");
+
+            return this;
+        }
+
+        public StRegFormPage setStateCity (String state, String city){
+            $("#state").click();
+            statecity.setState(state);
+            $("#city").click();
+            statecity.setCity(city);
+
+            return this;
+
+        }
+
+        public StRegFormPage setSubmit () {
+            $("#submit").click();
+
+            return this;
+        }
+
+        public StRegFormPage checkResult (String key, String value){
+
+            $(".table-responsive").$(byText(key)).parent()
+                    .shouldHave(text(value));
+            return this;
+
+
+        }
     }
 
-    public StRegFormPage setLastName(String lastName) {
-        $("#lastName").setValue(lastName);
-
-        return this;
-    }
-
-    public StRegFormPage setEmail(String email) {
-        $("#userEmail").setValue(email);
-
-        return this;
-    }
-
-    public StRegFormPage setCurrentAddress(String currentAddress) {
-        $("#currentAddress").setValue(currentAddress);
-
-        return this;
-    }
-
-    public StRegFormPage setMobile(String mobile) {
-        $("#userNumber").setValue(mobile);
-
-        return this;
-    }
-
-    public StRegFormPage setGender(String value) {
-        $("#genterWrapper").$(byText(value)).click();
-
-        return this;
-
-    }
-
-    public StRegFormPage setBirthDate(String day, String month, String year) {
-        $("#dateOfBirthInput").click();
-        calendar.setDate(day, month, year);
-
-        return this;
-    }
-
-    public StRegFormPage setSubjects(String value) {
-        $("#subjectsInput").setValue(value).pressEnter();
-
-        return this;
-    }
-
-    public StRegFormPage setHobbies(String value) {
-        $("#hobbiesWrapper").$(byText(value)).click();
-
-        return this;
-    }
-
-    public StRegFormPage setPicture(String value) {
-        $("#uploadPicture").uploadFromClasspath("1111.png");
-
-        return this;
-    }
-
-    public StRegFormPage setStateCity(String state, String city) {
-        $("#state").click();
-        statecity.setState(state);
-        $("#city").click();
-        statecity.setCity(city);
-
-        return this;
-
-    }
-
-    public StRegFormPage setSubmit() {
-        $("#submit").click();
-
-        return this;
-    }
-
-    public StRegFormPage checkResult(String key, String value) {
-
-        $(".table-responsive").$(byText(key)).parent()
-                .shouldHave(text(value));
-        return this;
-
-
-    }
-}
 
 
 
