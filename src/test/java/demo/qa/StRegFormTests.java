ac90e0950c91f960f20065dcfe678cef8f656d88
tonn1oo <ant.vlasov.87@gmail.com>
2022-05-12 11:33:46 +0300

    added owner

--- src/test/java/demo/qa/StRegFormTests.java
+++ src/test/java/demo/qa/StRegFormTests.java
@@ -1,9 +1,13 @@
 package demo.qa;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import com.github.javafaker.Faker;
+import config.CredentialsConfig;
 import demo.qa.pages.StRegFormPage;
 import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
@@ -15,12 +19,14 @@ import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class StRegFormTests {
 
+    static CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
 
     @BeforeAll
     static void setUp() {
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        Configuration.baseUrl = System.getProperty("base_url");
+        Configuration.browserSize = System.getProperty("browser_size");
+        Configuration.remote = "https://"+ config.login() + ":" + config.password() + "@" + System.getProperty("server_selenoid");
 
         /// Video
         DesiredCapabilities capabilities = new DesiredCapabilities();
